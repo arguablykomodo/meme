c84e5739921f5f8206778fe403f514e66ca7c7d7
SrKomodo <komodosr@gmail.com>
2018-03-17 00:28:14 -0300

    add some comments

--- meme.go
+++ meme.go
@@ -10,6 +10,7 @@ import (
 )
 
 func main() {
+	// Show help
 	if len(os.Args) < 2 || len(os.Args) > 3 || os.Args[1] == "help" || os.Args[1] == "--help" || os.Args[1] == "-h" {
 		println("Usage:")
 		println("  meme [input] [output]")
@@ -24,22 +25,26 @@ func main() {
 	output := os.Args[2]
 	flag.Parse()
 
+	// Validate input
 	if info, err := os.Stat(input); os.IsNotExist(err) || info.IsDir() {
 		println("Please input a valid meme")
 		return
 	}
 
+	// Validate output
 	if _, err := os.Stat(path.Dir(output)); os.IsNotExist(err) {
 		println("Please input a valid output file")
 		return
 	}
 
+	// Load meme
 	meme, err := toml.LoadFile(input)
 	if err != nil {
 		println("Error: Couldnt locate input meme")
 		panic(err.Error())
 	}
 
+	// Load image
 	image, err := gg.LoadImage(path.Join(path.Dir(input), meme.Get("image").(string)))
 	if err != nil {
 		println("Error: Couldnt load input meme")
@@ -48,6 +53,7 @@ func main() {
 
 	ctx := gg.NewContextForImage(image)
 
+	// Load font
 	if err = ctx.LoadFontFace(
 		meme.Get("font").(string),
 		float64(meme.Get("fontSize").(int64)),
@@ -56,9 +62,11 @@ func main() {
 		panic(err.Error())
 	}
 
+	// Draw text
 	ctx.SetRGB(0, 0, 0)
 	ctx.DrawStringAnchored("test", 310, 80, 0, 1)
 
+	// Save image
 	if err = ctx.SavePNG(output); err != nil {
 		println("Error: Couldnt save output meme")
 		panic(err.Error())
