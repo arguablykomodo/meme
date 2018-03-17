a83e6b18a8bd172a4f0a12043cabfd8617f08af2
SrKomodo <komodosr@gmail.com>
2018-03-16 23:27:01 -0300

    Add basic help command and input validation

--- meme.go
+++ meme.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"flag"
+	"os"
+	"path"
+)
+
 func main() {
-	println("Hello World!")
+	if len(os.Args) < 2 || len(os.Args) > 3 || os.Args[1] == "help" || os.Args[1] == "--help" || os.Args[1] == "-h" {
+		println("Usage:")
+		println("  meme [input] [output]")
+		println("    input = an input .toml file for a meme")
+		println("    output = a .png url to save the meme to")
+		println("Example:")
+		println("  meme ./foo.toml ./bar.png")
+		return
+	}
+
+	input := os.Args[1]
+	output := os.Args[2]
+	flag.Parse()
+
+	if info, err := os.Stat(input); os.IsNotExist(err) || info.IsDir() {
+		println("Please input a valid meme")
+		return
+	}
+
+	if _, err := os.Stat(path.Dir(output)); os.IsNotExist(err) {
+		println("Please input a valid output file")
+		return
+	}
+
+	println(input, output)
 }
